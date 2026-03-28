550712210deb2068a95e7f355f62094861eecb15
Edgar <mrrare3dg0019@gmail.com>
2026-03-28 19:39:54 -0400

    Model property

--- EdDealerAPI/Entities/Car.cs
+++ EdDealerAPI/Entities/Car.cs
@@ -4,6 +4,7 @@
     {
         public required int Id { get; set; }
         public string Brand { get; set; }
+        public string Model { get; set; }
         public string Vin { get; set; }
     }
 }
