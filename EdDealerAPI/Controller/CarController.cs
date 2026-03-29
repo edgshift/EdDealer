68560dd6c86f01a595b0a769e65b6681709336ea
Edgar <mrrare3dg0019@gmail.com>
2026-03-28 22:23:52 -0400

    Controller for Car entity is created.

---
+++ EdDealerAPI/Controller/CarController.cs
@@ -0,0 +1,19 @@
+﻿using EdDealerAPI.Data;
+using EdDealerAPI.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EdDealerAPI.Controller
+{
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+        public readonly Context context;
+
+        public CarController(Context context)
+        {
+            this.context = context;
+        }
+
+    }
+}
