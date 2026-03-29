16e773e9dec3f5d50af8caa9dc158ee4f9feada9
Edgar <mrrare3dg0019@gmail.com>
2026-03-28 21:53:55 -0400

    String Connection created

--- EdDealerAPI/Program.cs
+++ EdDealerAPI/Program.cs
@@ -1,8 +1,13 @@
+using EdDealerAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<Context>(opciones=> opciones.UseSqlServer());
 
 
 var app = builder.Build();
 
+app.MapControllers();
 
 app.Run();
